ebeb3bdb0f13f9dbff678ba0d5681a74b12278b7
Bytecm <80549807+Bytecm@users.noreply.github.com>
2023-03-12 02:44:05 +0100

    removed depricated setting

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1447,9 +1447,6 @@
   // On the Info Screen, display XY with one decimal place when possible
   //#define LCD_DECIMAL_SMALL_XY
 
-  // Add an 'M73' G-code to set the current percentage
-  #define LCD_SET_PROGRESS_MANUALLY
-
   // Show the E position (filament used) during printing
   //#define LCD_SHOW_E_TOTAL
 
